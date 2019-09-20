744b1e322841fadb8cb8131ab48ac0edf374f7d3
Mau Zsofia Abraham <mau.palantir@gmail.com>
2019-09-20 21:22:45 +0200

    test

--- src/draw/DrawToolbar.js
+++ src/draw/DrawToolbar.js
@@ -5,7 +5,7 @@ L.Toolbar2.DrawToolbar = L.Toolbar2.Control.extend({
 			L.Toolbar2.DrawAction.Polyline,
 			L.Toolbar2.DrawAction.Marker,
 			L.Toolbar2.DrawAction.Rectangle,
-			L.Toolbar2.DrawAction.Circle
+//			L.Toolbar2.DrawAction.Circle
 		],
 		className: 'leaflet-draw-toolbar'
 	}
